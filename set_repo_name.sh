ddcc699588e2e73a3c29d613791cd11fc1b9db40
Pramod-gi <pramodsmsit@gmail.com>
2023-10-09 18:57:14 +0530

    test

--- set_repo_name.sh
+++ set_repo_name.sh
@@ -6,6 +6,8 @@ REPOSITORY_NAME=$(gcloud artifacts repositories list --location=$LOCATION --form
 echo $REPOSITORY_NAME
 
 # Export the REPOSITORY_NAME as an environment variable
-export REPOSITORY_NAME
+# export REPOSITORY_NAME
+ENV REPOSITORY_NAME=$REPOSITORY_NAME
+
 
 chmod +x set_repo_name.sh
