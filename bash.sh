de4bba1afcbe8490b779c8ed805b987c6fbfb935
Pramod-gi <pramodsmsit@gmail.com>
2023-10-09 18:44:50 +0530

    bash

--- bash.sh
+++ bash.sh
@@ -3,6 +3,8 @@
 # Use gcloud CLI to fetch the repository name from Artifact Registry
 REPOSITORY_NAME=$(gcloud artifacts repositories list --location=$LOCATION --format="value(name)")
 
+echo $REPOSITORY_NAME
+
 # Export the REPOSITORY_NAME as an environment variable
 export REPOSITORY_NAME
 
