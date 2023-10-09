c1529b6892b5e24e4f17e2c7cffeb94991b4abc4
Pramod-gi <pramodsmsit@gmail.com>
2023-10-09 18:58:08 +0530

    test

--- set_repo_name.sh
+++ set_repo_name.sh
@@ -7,7 +7,7 @@ echo $REPOSITORY_NAME
 
 # Export the REPOSITORY_NAME as an environment variable
 # export REPOSITORY_NAME
-ENV REPOSITORY_NAME=$REPOSITORY_NAME
+export REPOSITORY_NAME=$REPOSITORY_NAME
 
 
 chmod +x set_repo_name.sh
