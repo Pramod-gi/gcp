4a2430d17a22fea556b62a75c6246f168b16fc0f
pramodg24 <pramodsmsit@gmail.com>
2023-10-11 18:37:25 +0530

    latest changes

---
+++ config/config.js
@@ -0,0 +1,77 @@
+const { Storage } = require('@google-cloud/storage');
+const axios= require('axios');
+
+/**
+   * @desc Function to read the config file's contents from GCS.
+   * @param nothing
+   * @return bool - success or failure
+   */
+
+async function gcp_Read_Configuration() {
+  try {
+    const currentConfig = global?.gConfig;
+    console.log("global config contents ===>", currentConfig);
+    
+    
+    const {projectId,current_BUCKET_NAME} =currentConfig; // Assuming the project ID is in the response global config
+    console.log("in quickstart gcp_Read_Configuration", {projectId,current_BUCKET_NAME});
+      console.log(`Current project ID in global config: ${projectId}`);
+
+      if (!projectId) {
+        console.log(`Project ${projectId} does not exist.`);
+        return global.gConfig;
+      }
+
+      const storage = new Storage({ projectId });
+      const [buckets] = await storage.getBuckets();
+                // Check if the current_BUCKET_NAME exists in the list of buckets
+    
+      console.log("bucket name is ====>",current_BUCKET_NAME)
+      const bucketExists = buckets.some(bucket => bucket.name ===current_BUCKET_NAME );
+
+      if (bucketExists) {
+        console.log(`Bucket ${current_BUCKET_NAME} exists.`);
+        const [files] = await storage.bucket(current_BUCKET_NAME).getFiles();
+        console.log('Files in bucket:');
+
+        for (const file of files) {
+          console.log(file.name);
+          const stream = await storage.bucket(current_BUCKET_NAME).file(file.name).createReadStream();
+          let configFileData = '';
+
+          stream.on('data', function (chunk) {
+            configFileData += chunk;
+          });
+
+          stream.on('end', () => {
+            const parsedConfig = JSON.parse(configFileData); // Parse the JSON data
+            console.log('Files in parsedConfig bucket:', parsedConfig);
+
+            if (JSON.stringify(currentConfig["config"]) !== JSON.stringify(parsedConfig)) {
+              console.log('Config file updated.');
+              currentConfig["config"] = parsedConfig;
+              global.gConfig = { ...currentConfig }; // Avoid garbage memory
+              console.log("After Updated config ===>", global.gConfig);
+            } else {
+              console.log("No update required for configuration.");
+            }
+          });
+        }
+
+        return global.gConfig;
+      } else {
+        console.log(`Bucket ${current_BUCKET_NAME} does not exist.`);
+        return global.gConfig;
+      }
+    
+    
+  } catch (error) {
+    console.log("Error in gcp_Read_Configuration ===>", error);
+    return global.gConfig;
+  }
+}
+
+
+module.exports = {
+  gcp_Read_Configuration,
+};
--- server.js
+++ server.js
@@ -3,9 +3,14 @@ const app = express();
 const cors = require('cors');
 const morgan = require('morgan');
 const nocache = require('nocache');
-const axios = require('axios');
 require('dotenv').config();
-global.projectId='';
+
+const {_BUCKET_NAME:current_BUCKET_NAME,_PROJECT_ID:pojectId} = process.env;
+
+console.log("============= ENV ======>",process.env, { pojectId, current_BUCKET_NAME})
+global.gConfig = { config:{},current_BUCKET_NAME ,pojectId};
+
+
 //* **************cache handling**************8 */
 app.use(nocache());
 app.disable('view cache');
@@ -17,63 +22,89 @@ app.use(morgan('dev', {}));
 app.use(express.json({ limit: '10mb' }));
 
 app.use((error, req, res, next) => {
-    if (error instanceof SyntaxError) {
-        return res.status(400).send({ status: 400, success: false, message: 'Bad request.' });
-    }
-    next();
+  if (error instanceof SyntaxError) {
+    return res.status(400).send({ status: 400, success: false, message: 'Bad request.' });
+  }
+  next();
 });
 
-async function gcp_Read_Projectdetails() {
-    try {
-        const nodeAppEndpoint = "http://metadata.google.internal/computeMetadata/v1/project/project-id";
-        console.log("nodeAppEndpointnodeAppEndpoint=======================>", nodeAppEndpoint);
-        const response = await axios.get(nodeAppEndpoint, {
-            headers: {
-                'Metadata-Flavor': 'Google',
-            },
+process
+  .on('unhandledRejection', (reason, p) => {
+    console.error(`Unhandled Rejection at: Promise', ${p}, reason: ${reason}`);
+    process.exit(1);
+  })
+  .on('uncaughtException', (err) => {
+    console.error(`${new Date().toUTCString()} uncaughtException:${err.message}`);
+    console.error(`${err.stack}`);
+    process.exit(1);
+  });
+
+
+//   async function gcp_Read_Projectdetails() {
+//     try {
+//         const nodeAppEndpoint = "http://metadata.google.internal/computeMetadata/v1/project/project-id";
+//         console.log("nodeAppEndpointnodeAppEndpoint=======================>", nodeAppEndpoint);
+//         const response = await axios.get(nodeAppEndpoint, {
+//             headers: {
+//                 'Metadata-Flavor': 'Google',
+//             },
+//         });
+//         if (response.status === 200) {
+//             const project_Id = response.data; // Assuming the project ID is in the response body
+//             console.log(`Current project ID=======>: ${project_Id}`);
+//             if (project_Id) {
+//                 global.gConfig.pojectId = project_Id;
+//                 console.log(`Project exists.`);
+//                 return true;
+//             } else {
+//                 console.log(`Project does not exist.`);
+//                 return false;
+//             }
+//         } else {
+//             console.error('Failed to fetch project ID:', response.status);
+//             return false;
+//         }
+//     } catch (error) {
+//         console.log(error);
+//         return false;
+//     }
+// }
+//  gcp_Read_Projectdetails()
+
+      try {
+        app.listen(7000, async () => {
+          try {           
+            let gcp_bucket = await require('./config/config').gcp_Read_Configuration();
+            app.get('/config',(req,res)=>{
+              try {
+                return res.status(200).send(global.gConfig)
+              } catch (error) {    
+                return res.status(500).send({
+                  status:false,
+                  message:"Internal server error"
+                })
+              }
+             })
+             app.post('/notify',async(req,res)=>{
+              try {
+                let events= req.body;
+                console.log("fetch notify data====>",events);
+                  let output = await require('./config/config').gcp_Read_Configuration();
+                return res.status(200).send({status:true,events})
+              } catch (error) {
+                return res.status(500).send({
+                  status:false,
+                  message:"Internal server error"
+                })
+              }
+             })
+    
+            console.log(`Server is running on port: 7000`);
+          } catch (error) {
+            console.log('Could not connect to the config server: ', error);
+          }
         });
-        if (response.status === 200) {
-            const project_Id = response.data; // Assuming the project ID is in the response body
-            console.log(`Current project ID: ${project_Id}`);
-            if (project_Id) {
-                global.projectId = project_Id;
-                console.log(`Project exists.`);
-                return true;
-            } else {
-                console.log(`Project does not exist.`);
-                return false;
-            }
-        } else {
-            console.error('Failed to fetch project ID:', response.status);
-            return false;
-        }
-    } catch (error) {
-        console.log(error);
-        return false;
-    }
-}
-
-
-app.get('/details', (req, res) => {
-    try {
-        return res.status(200).send({'projectId':projectId})
-    } catch (error) {
-        return res.status(500).send(error)
-    }
-})
-
-
-
-try {
-        app.listen(8000, async () => {
-            try {
-                let gcp_bucket = await gcp_Read_Projectdetails();
-                console.log(`Server is running on port: 8000`);
-            } catch (error) {
-                console.log('Could not connect to the config server: ', error);
-            }
-        });
-    } catch (e) {
+      } catch (e) {
         console.error(`Server is not responding.${e}`);
-    }
+      }
 
