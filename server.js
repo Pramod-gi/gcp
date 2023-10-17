46e323ffa4a4d424fe9f169b3c77ce4a421f584c
pramodg24 <pramodsmsit@gmail.com>
2023-10-17 18:29:26 +0530

    latest changes

--- server.js
+++ server.js
@@ -39,42 +39,10 @@ process
     process.exit(1);
   });
 
-
-//   async function gcp_Read_Projectdetails() {
-//     try {
-//         const nodeAppEndpoint = "http://metadata.google.internal/computeMetadata/v1/project/project-id";
-//         console.log("nodeAppEndpointnodeAppEndpoint=======================>", nodeAppEndpoint);
-//         const response = await axios.get(nodeAppEndpoint, {
-//             headers: {
-//                 'Metadata-Flavor': 'Google',
-//             },
-//         });
-//         if (response.status === 200) {
-//             const project_Id = response.data; // Assuming the project ID is in the response body
-//             console.log(`Current project ID=======>: ${project_Id}`);
-//             if (project_Id) {
-//                 global.gConfig.pojectId = project_Id;
-//                 console.log(`Project exists.`);
-//                 return true;
-//             } else {
-//                 console.log(`Project does not exist.`);
-//                 return false;
-//             }
-//         } else {
-//             console.error('Failed to fetch project ID:', response.status);
-//             return false;
-//         }
-//     } catch (error) {
-//         console.log(error);
-//         return false;
-//     }
-// }
-//  gcp_Read_Projectdetails()
-
       try {
-        app.listen(PORT, async () => {
-          try {           
-            //let gcp_bucket = await require('./config/config').gcp_Read_Configuration();
+        app.listen( PORT, async () => {
+          try {    
+            let gcp_bucket = await require('./config/config').gcp_Read_Configuration();
             app.get('/config',(req,res)=>{
               try {
                 return res.status(200).send(global.gConfig)
@@ -85,26 +53,23 @@ process
                 })
               }
              })
-
              //API end point trigger by cloud function once bucket file changes/over-write 
              app.post('/notify',async(req,res)=>{
               try {
                 let events= req.body;
-                const { bucketName, name, data } = req.body;
+                const { bucketName, name,jsonData } = req.body;
                 const currentConfig = global?.gConfig;
                 console.log("global config contents ===>", currentConfig);
-                console.log("fetch notify data====>",events);
-                  //let output = await require('./config/config').gcp_Read_Configuration();
-
-                  if (JSON.stringify(currentConfig["config"]) !== JSON.stringify(events.jsonData)) {
+                console.log("fetch notify data====>",jsonData);
+                  if (JSON.stringify(currentConfig["config"]) !== JSON.stringify(jsonData)) {
                     console.log('Config file updated.');
-                    currentConfig["config"] = events.jsonData;
+                    currentConfig["config"] = jsonData;
                     global.gConfig = { ...currentConfig }; // Avoid garbage memory
                     console.log("After Updated config ===>", global.gConfig);
                   } else {
                     console.log("No update required for configuration.");
                   }
-                return res.status(200).send({status:true,events})
+                return res.status(200).send({status:true,jsonData})
               } catch (error) {
                 return res.status(500).send({
                   status:false,
@@ -112,8 +77,7 @@ process
                 })
               }
              })
-    
-            console.log(`Server is running on port: 7000`);
+            console.log(`Server is running on port: ${PORT}`);
           } catch (error) {
             console.log('Could not connect to the config server: ', error);
           }
